782db576e7f7b2b1ff7114acc6a1a3bb695204db
Ankit Kumar <iamankit7667@gmail.com>
2023-05-07 18:01:49 +0530

    Update

--- codescope/codescope_while/prime_factor.py
+++ codescope/codescope_while/prime_factor.py
@@ -1,13 +1,18 @@
-a=int(input("Enter a number :"))
-x=a
-b=0
+a=int(input("Enter a number: "))
+x=2
+b=2
 
-while(x>=2):
-    if(a%x==0):
-        b=x-1
-        while(b>1):
-            if(x%b!=0):
-                print(x," is a prime factor ")
-            b=b-1
-    x=x-1
+while(x>1):
+    if(a%x==0 and x!=a):
+        print(x)
+        b=x
+
+        while(b>0):
+            if(x%b==0 and b<x):
+                print("Not a prime factor")
                 
+            if(b==x):
+                print(x," is a prime factor")
+            b=b+1
+
+    x=x+1
